3b01f182b2645aba16a5f754f78852dba6061409
cbpumpcoaching <cbpumpcoaching@gmail.com>
2026-01-16 15:26:32 +0000

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Online Coaching Platform</h1>
+      <p>Your website is live.</p>
+    </main>
+  );
+}
