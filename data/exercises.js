0fe5ff46d364eab8c02e8a5a24e829b2e183ecae
cbpumpcoaching <cbpumpcoaching@gmail.com>
2026-01-21 16:59:00 +0000

    Update exercises.js

--- data/exercises.js
+++ data/exercises.js
@@ -1,1 +1,45 @@
-
+// /data/exercises.js
+export const EXERCISES = [
+  {
+    id: "bw-squat",
+    name: "Bodyweight Squat",
+    movement: "squat",
+    equipmentTier: ["bodyweight", "bands", "dumbbells", "fullGym"],
+    minLevel: "beginner",
+  },
+  {
+    id: "db-goblet-squat",
+    name: "Dumbbell Goblet Squat",
+    movement: "squat",
+    equipmentTier: ["dumbbells", "fullGym"],
+    minLevel: "beginner",
+  },
+  {
+    id: "gym-back-squat",
+    name: "Barbell Back Squat",
+    movement: "squat",
+    equipmentTier: ["fullGym"],
+    minLevel: "intermediate",
+  },
+  {
+    id: "bw-pushup",
+    name: "Push-Up",
+    movement: "push",
+    equipmentTier: ["bodyweight", "bands", "dumbbells", "fullGym"],
+    minLevel: "beginner",
+  },
+  {
+    id: "db-row",
+    name: "One-Arm Dumbbell Row",
+    movement: "pull",
+    equipmentTier: ["dumbbells", "fullGym"],
+    minLevel: "beginner",
+  },
+  {
+    id: "plank",
+    name: "Plank",
+    movement: "core",
+    equipmentTier: ["bodyweight", "bands", "dumbbells", "fullGym"],
+    minLevel: "beginner",
+  },
+];
