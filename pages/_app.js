5070de52946cbb5938287d97ed96994a67c14a6a
cbpumpcoaching <cbpumpcoaching@gmail.com>
2026-01-21 11:43:26 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import "../styles/globals.css";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      {/* Simple navbar on every page */}
+      <nav
+        style={{
+          padding: "16px",
+          borderBottom: "1px solid #ddd",
+          display: "flex",
+          gap: "20px",
+          alignItems: "center",
+        }}
+      >
+        <Link href="/">Home</Link>
+        <Link href="/pricing">Pricing</Link>
+        <Link href="/members">Members</Link>
+      </nav>
+
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
